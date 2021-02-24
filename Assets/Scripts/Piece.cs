7668d40ee4d468cb4b2d3fc0196a7b25658c6b8c
FoxerLee <foxerlee1@gmail.com>
2021-02-24 16:51:29 -0500

    upload final version&README

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -15,23 +15,23 @@ public class Piece : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 og = target.transform.position;
-        Quaternion og_rot = target.transform.rotation;
+        // Vector3 og = target.transform.position;
+        // Quaternion og_rot = target.transform.rotation;
 
-        int id = 1;
-        for (int i = -half_horizen; i <= half_horizen; i++) {
-            for (int j = -half_vertical; j <= half_vertical; j++) {
-                if (i == 0 && j == 0)
-                {
-                    id += 1;
-                } 
-                // continue;
-                Vector3 add = new Vector3(i * (width + gap) + j * ((width + gap) / 2), j * height, 0);
-                GameObject newObj = Instantiate(target, og + add, og_rot, mask.transform);
-                newObj.name = "hex(" + i +","+j+ ")," + id;
-                id += 1;
-            }
-        }
+        // int id = 1;
+        // for (int i = -half_horizen; i <= half_horizen; i++) {
+        //     for (int j = -half_vertical; j <= half_vertical; j++) {
+        //         if (i == 0 && j == 0)
+        //         {
+        //             id += 1;
+        //         } 
+        //         // continue;
+        //         Vector3 add = new Vector3(i * (width + gap) + j * ((width + gap) / 2), j * height, 0);
+        //         GameObject newObj = Instantiate(target, og + add, og_rot, mask.transform);
+        //         newObj.name = "hex(" + i +","+j+ ")," + id;
+        //         id += 1;
+        //     }
+        // }
     }
 
     // Update is called once per frame
---
+++ README.md
@@ -0,0 +1,14 @@
+# Six2Six
+
+Idea comes from: Five in a Row (Gomoku)
+
+itch.io link: https://itch.io/game/edit/933036
+
+## Rule
+
+- Players alternate turns placing a piece on an empty intersection.
+- Try to build a row/circle/triangle of six pieces
+- When a row/circle/triangle is finished, player can get the sum of points on the six pieces
+- 2 pieces with 6 points, 3 pieces with 5, 3 pieces with 4, 5 pieces with 3, 5 pieces with 2, 6 pieces with 1
+- All pieces are used / No more place for pieces, game is over
+
