2d4dc6fdac8aa6300a341680cb9723de615b2363
vy538 <good5538@gmail.com>
2021-04-12 18:08:59 -0400

    add tab animation
    have bugs

---
+++ Assets/Scripts/ItemEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ItemEvent : MonoBehaviour
+{
+    public UnityEvent hoverStartEvent;
+    public UnityEvent hoverEndEvent;
+
+
+    void StartHover()
+    {
+        print("invoke start hover");
+        hoverStartEvent.Invoke();
+    }
+
+    void EndHover()
+    {
+        print("invoke end hover");
+        hoverEndEvent.Invoke();
+    }
+}
