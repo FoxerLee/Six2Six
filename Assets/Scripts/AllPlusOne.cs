8ea707cdb36a1fda731e1e44111d96e598c18f57
FoxerLee <foxerlee1@gmail.com>
2021-04-19 23:27:44 -0400

    combine score & power-up system for all nei+1 effect

--- Assets/Scripts/AllPlusOne.cs
+++ Assets/Scripts/AllPlusOne.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AllPlusOne : PowerUp
 {
@@ -13,5 +16,46 @@ public class AllPlusOne : PowerUp
     public override void takeEffect(GameObject board)
     {
         Debug.Log("This is a +1 power-up. Location: " + board.name);
+
+        var idx = board.name.Substring(3).Split(',');
+
+        var xs = idx[0].Substring(1);
+        var ys = idx[1].Substring(0, idx[1].Length - 1);
+
+        int x = StringToInt(xs);
+        int y = StringToInt(ys);
+
+        int id = StringToInt(idx[2]);
+
+        Tuple<int, int>[] neis = {Tuple.Create(x, y+1), Tuple.Create(x, y-1),
+                                Tuple.Create(x+1, y), Tuple.Create(x-1, y),
+                                Tuple.Create(x-1, y+1), Tuple.Create(x+1, y-1)};
+
+        Text neiText;
+
+        foreach (Tuple<int, int> neIdx in neis)
+        {
+            int neiX = neIdx.Item1;
+            int neiY = neIdx.Item2;
+            // effect only works for neighbors which already been placed.
+            // if (ScoreManager.instance.chessboard.ContainsKey(Tuple.Create(neiX, neiY)))
+            if (ScoreManager.instance.isScored[Tuple.Create(neiX, neiY)] == true)
+            {
+                string curPlayer = ScoreManager.instance.chessboard[Tuple.Create(neiX, neiY)].Item1;
+                int curScore = ScoreManager.instance.chessboard[Tuple.Create(neiX, neiY)].Item2 + 1;
+                int neId = ScoreManager.instance.chessboard[Tuple.Create(neiX, neiY)].Item3;
+                ScoreManager.instance.chessboard[Tuple.Create(neiX, neiY)] = Tuple.Create(curPlayer, curScore, neId);
+
+                // update UI score
+                neiText = GameObject.Find("hex("+neiX.ToString()+","+neiY.ToString()+"),"+neId.ToString()).GetComponentInChildren<Text>();
+                neiText.text = "" + curScore;
+            }
+        }
     }
+
+    private int StringToInt(string s)
+    {
+        return Int32.Parse(s);
+    }
+
 }
