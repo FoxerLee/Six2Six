da60f53f99c264b4982b74ffd4b5dfea462cfc5b
Un-Jarvis <huang.2089@osu.edu>
2021-04-25 14:55:11 -0400

    Update Drag.cs

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -7,9 +7,14 @@ using UnityEngine.EventSystems;
 public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
-    [SerializeField] private Canvas canvas;
     public GameObject dropTab;
+
+    private Canvas canvas;
     private GameObject draggedCard;
+
+    void Start() {
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
